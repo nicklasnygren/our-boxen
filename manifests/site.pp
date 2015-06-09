975692af82b9691bd9e65bc5ae2f59fe3386954b
Nicklas Nygren <nicklasnygren@Nicklass-MacBook-Pro.local>
2015-06-09 21:38:50 +0200

    Add some bare necessities

--- manifests/site.pp
+++ manifests/site.pp
@@ -58,6 +58,37 @@ node default {
   include hub
   include nginx
 
+  include iterm2::stable
+  include macvim
+  include cmake
+  include skype
+  include java
+  include seil
+  include tmux
+  include xquartz
+  include python
+
+  # Seil options
+  include seil::login_item
+
+  # Iterm settings
+  include iterm2::colors::solarized_dark
+
+  # OSX settings
+  include osx::global::disable_key_press_and_hold
+  include osx::universal_access::ctrl_mod_zoom
+  include osx::finder::unhide_library
+  include osx::dock::disable_dashboard
+  include osx::no_network_dsstores
+  include osx::global::expand_save_dialog
+  include osx::global::disable_autocorrect
+  include osx::global::tap_to_click
+  include osx::keyboard::capslock_to_control
+  include osx::finder::show_all_filename_extensions
+  include osx::finder::show_warning_before_changing_an_extension
+  include osx::disable_app_quarantine
+  include osx::sound::interface_sound_effects
+
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
